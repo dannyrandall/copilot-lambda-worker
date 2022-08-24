0aad3809e0089c7cad53fbe2341bdddf1dbb2d4f
Daniel Randall <10566468+dannyrandall@users.noreply.github.com>
2022-08-24 12:43:07 -0700

    publish incoming requests

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 'use strict';
 
-// const { randomTopicName } = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
+const aws = require("aws-sdk");
+const sns = new aws.SNS();
 
-const app = require('express')();
-app.set('trust proxy', true);
+const { requests } = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
+console.log(`Sending requests to ${requests}`);
+
+const app = require("express")();
+app.set("trust proxy", true);
 
 app.get("/healthz", (req, res) => {
 	res.status(200).send();
 });
 
-app.get("/*", (req, res) => {
+app.get("/*", async (req, res) => {
 	console.log(`Handling ${req.method} request from ${req.ip} to ${req.url}`);
-	res.status(200).send(`Hello ${req.ip}!`);
+
+	try {
+		await sns.publish({
+			TopicArn: requests,
+			Message: JSON.stringify({
+				method: req.method,
+				source: req.ip,
+				url: req.url
+			}),
+		}).promise();
+
+		res.status(200).send(`Hello ${req.ip}!`);
+	} catch (err) {
+		console.warn("publish message:", err);
+		res.status(500).send(`an error occured`);
+	}
 });
 
 app.listen(8080, () => {
-	console.log('Listening on :8080');
+	console.log("Listening on :8080");
 });
